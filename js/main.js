0115a8af1ffa7f7c617673c9997de732b5bcba19
TatianaLiholet <37273006+TatianaLiholet@users.noreply.github.com>
2018-03-17 14:50:58 +0200

    second commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+   $('.firstSlider').slick({
+        infinite: true,
+  		speed: 1000,
+  		fade: true,
+  		cssEase: 'linear',
+  		arrows: false,
+  		autoplay: true
+   });
+});
+
+
+$(function(){
+   $('.container_baner').slick({
+        infinite: true,
+  		speed: 1000,
+  		fade: true,
+  		cssEase: 'linear',
+  		arrows: true,
+  		autoplay: true
+   });
+});
